f5184ba1c9509012c87cc02e4aa5a579ea3764a0
Ernesto Rodriguez <erodrigueztoimil@gmail.com>
2020-01-21 15:01:22 -0500

    worked on the html structure and basic styling

---
+++ assets/questions.js
@@ -0,0 +1,25 @@
+var questions = [
+  {
+    title: 'first question',
+    choices: ['string', 'string','string', 'string', 'string'],
+    answer: ''
+  },
+  {
+    title: 'second question',
+    choices: ['string', 'string','string', 'string', 'string'],
+    answer: ''
+  },
+  {
+    title: 'third question',
+    choices: ['string', 'string','string', 'string', 'string'],
+    answer: ''
+  },
+  {
+    title: 'fourth question',
+    choices: ['string', 'string','string', 'string', 'string'],
+    answer: ''
+  },
+];
+
+
+export default questions;
